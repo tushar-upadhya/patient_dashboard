6ba62f09883f50a3a10dcce534f631d6839089f2
tushar upadhyay <tusharupadhyay690@gmail.com>
2025-06-02 13:38:18 +0530

    feat: enhance MedicineTable with ScrollArea and add additional medicines to state

--- src/components/MedicineTable.tsx
+++ src/components/MedicineTable.tsx
@@ -1,6 +1,7 @@
 import type { PatientState } from "@/types/patient";
 import { useSelector } from "react-redux";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { ScrollArea } from "./ui/scroll-area"; // Import ScrollArea from shadcn/ui
 import {
     Table,
     TableBody,
@@ -11,38 +12,68 @@ import {
 } from "./ui/table";
 
 export const MedicineTable: React.FC = () => {
-    const { medicines } = useSelector(
-        (state: { patient: PatientState }) => state.patient
+    const medicines = useSelector(
+        (state: { patient: PatientState }) => state.patient.medicines
     );
 
     return (
         <Card>
             <CardHeader>
-                <CardTitle>Medicine Details</CardTitle>
+                <CardTitle id="medicine-details-title">
+                    Medicine Details
+                </CardTitle>
             </CardHeader>
             <CardContent>
-                <Table>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead>Medicine Name</TableHead>
-                            <TableHead>Dosage</TableHead>
-                            <TableHead>Frequency</TableHead>
-                            <TableHead>Duration</TableHead>
-                            <TableHead>Prescribed By</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {medicines.map((medicine) => (
-                            <TableRow key={medicine.id}>
-                                <TableCell>{medicine.name}</TableCell>
-                                <TableCell>{medicine.dosage}</TableCell>
-                                <TableCell>{medicine.frequency}</TableCell>
-                                <TableCell>{medicine.duration}</TableCell>
-                                <TableCell>{medicine.prescribedBy}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
+                <ScrollArea className="h-48 w-full">
+                    <div className="overflow-x-auto min-w-[600px]">
+                        <Table aria-describedby="medicine-details-title">
+                            <caption className="sr-only">
+                                List of prescribed medicines with details
+                            </caption>
+                            <TableHeader>
+                                <TableRow>
+                                    <TableHead>Medicine Name</TableHead>
+                                    <TableHead>Dosage</TableHead>
+                                    <TableHead>Frequency</TableHead>
+                                    <TableHead>Duration</TableHead>
+                                    <TableHead>Prescribed By</TableHead>
+                                </TableRow>
+                            </TableHeader>
+                            <TableBody>
+                                {medicines.length > 0 ? (
+                                    medicines.map((medicine) => (
+                                        <TableRow key={medicine.id}>
+                                            <TableCell>
+                                                {medicine.name}
+                                            </TableCell>
+                                            <TableCell>
+                                                {medicine.dosage}
+                                            </TableCell>
+                                            <TableCell>
+                                                {medicine.frequency}
+                                            </TableCell>
+                                            <TableCell>
+                                                {medicine.duration}
+                                            </TableCell>
+                                            <TableCell>
+                                                {medicine.prescribedBy}
+                                            </TableCell>
+                                        </TableRow>
+                                    ))
+                                ) : (
+                                    <TableRow>
+                                        <TableCell
+                                            colSpan={5}
+                                            className="text-center italic text-gray-500"
+                                        >
+                                            No medicines prescribed
+                                        </TableCell>
+                                    </TableRow>
+                                )}
+                            </TableBody>
+                        </Table>
+                    </div>
+                </ScrollArea>
             </CardContent>
         </Card>
     );
