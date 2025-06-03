e965451237c8f705104c0bf71aa4c9548516196b
tushar upadhyay <tusharupadhyay690@gmail.com>
2025-06-03 10:49:31 +0530

    refactor: update BottomTabs component structure and improve styling for TabsList

--- src/components/layout/BottomTabs.tsx
+++ src/components/layout/BottomTabs.tsx
@@ -50,7 +50,7 @@ export const BottomTabs: React.FC = () => {
     };
 
     return (
-        <div>
+        <>
             {/* Mobile & Tablet View */}
             <div className="lg:hidden px-4 py-3 bg-muted w-full fixed bottom-0 left-0 z-10 shadow-md">
                 <Select value={currentTab} onValueChange={handleNavigate}>
@@ -73,12 +73,12 @@ export const BottomTabs: React.FC = () => {
             {/* Desktop View */}
             <div className="hidden lg:block">
                 <Tabs value={currentTab} className="w-full">
-                    <TabsList className="flex items-center justify-center gap-6 px-6 py-3 bg-muted rounded-lg max-w-screen-2xl mx-auto mt-4">
+                    <TabsList className="flex items-center justify-center gap-6  py-3 bg-muted/80 rounded-lg max-w-screen-2xl mx-auto mt-4">
                         {tabItems.map(({ label, icon: Icon, path }) => (
                             <TabsTrigger
                                 key={path}
                                 value={normalizeTabValue(label)}
-                                className="flex items-center gap-2 px-5 py-2 text-sm whitespace-nowrap"
+                                className="flex items-center gap-2 px-5 py-4 text-sm whitespace-nowrap"
                                 onClick={() => navigate(path)}
                             >
                                 <Icon className="h-5 w-5" />
@@ -88,6 +88,6 @@ export const BottomTabs: React.FC = () => {
                     </TabsList>
                 </Tabs>
             </div>
-        </div>
+        </>
     );
 };
